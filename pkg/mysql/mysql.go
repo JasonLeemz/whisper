78d36f3c5633b00c81952474faa7b63177d9c5f7
limingze <limingze610@gmail.com>
2023-09-21 18:47:05 +0800

    up

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 	"whisper/pkg/config"
 	"whisper/pkg/log"
 )
 
-var DB *gorm.DB
+var (
+	DB *gorm.DB
+)
 
 func Init() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -19,9 +22,20 @@ func Init() {
 		config.GlobalConfig.Database.DB,
 	)
 	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+	DB, err = gorm.Open(mysql.New(mysql.Config{
+		DSN: dsn,
+	}), &gorm.Config{
 		Logger: log.GLogger,
 	})
+
+	sqlDB, err := DB.DB()
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
 	if err != nil {
 		panic(err)
 	}
--- web/template/whisper/src/main.js
+++ web/template/whisper/src/main.js
@@ -7,8 +7,8 @@ import './assets/main.scss'
 
 import axios from 'axios';
 // 设置 Axios 全局配置
-// axios.defaults.baseURL = '';
-axios.defaults.baseURL = 'http://192.168.31.218:8123';
+axios.defaults.baseURL = '';
+// axios.defaults.baseURL = 'http://192.168.31.218:8123';
 
 
 const app = createApp(App)
